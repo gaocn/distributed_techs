8bc929f7ad406ea313be6b51d82501094fe4f789
高文文 <gaowenwencn@163.com>
2018-07-31 10:45:22 +0800

    Curator实战：统一配置服务

---
+++ zk-in-action/src/main/java/govind/zkinaction/curator/CuratorUsage.java
@@ -0,0 +1,279 @@
+package govind.zkinaction.curator;
+
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.BackgroundCallback;
+import org.apache.curator.framework.api.CuratorEvent;
+import org.apache.curator.framework.api.CuratorWatcher;
+import org.apache.curator.framework.recipes.cache.*;
+import org.apache.curator.framework.state.ConnectionState;
+import org.apache.curator.framework.state.ConnectionStateListener;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Id;
+import org.apache.zookeeper.data.Stat;
+import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CuratorUsage {
+    private static Logger logger  = LoggerFactory.getLogger(CuratorUsage.class);
+    final static String CONN_STR="10.233.87.241:9080,10.233.87.54:9080";
+    final static int SESSION_TIMEOUT = 2000;
+    final static int CONN_TIMEOUT = 5000;
+
+    public static void main(String[] args) throws  Exception{
+        ExponentialBackoffRetry backoffRetry = new ExponentialBackoffRetry(1000, 10);
+
+//        CuratorFramework client = CuratorFrameworkFactory.newClient(
+//                CONN_STR,
+//                SESSION_TIMEOUT,
+//                CONN_TIMEOUT,
+//                backoffRetry
+//        );
+
+        /**
+         * ACL有IP授权和用户名密码访问的模式
+         */
+        ACL  aclRoot = new ACL(ZooDefs.Perms.ALL,
+                new Id("digest",
+                        DigestAuthenticationProvider.generateDigest("root:root")));
+        List<ACL> acls =  new ArrayList<ACL>();
+        acls.add(aclRoot);
+
+        CuratorFramework client = CuratorFrameworkFactory.builder()
+                .connectString(CONN_STR)
+                .connectionTimeoutMs(CONN_TIMEOUT)
+                .sessionTimeoutMs(SESSION_TIMEOUT)
+                .retryPolicy(backoffRetry)
+                .authorization("digest", "gao:gao".getBytes())
+                .build();
+
+        //连接状态监听
+        client.getConnectionStateListenable().addListener(
+                new ConnectionStateListener() {
+                    public void stateChanged(CuratorFramework client, ConnectionState newState) {
+                        logger.info("Connection Stated Changed: [{}]", newState);
+                    }
+                }
+        );
+
+        client.start();
+
+        String result = null;
+
+        result = client.create()
+                .creatingParentsIfNeeded()
+                .withMode(CreateMode.EPHEMERAL)
+                .withACL(acls)
+                .forPath("/acls", "acl".getBytes());
+        logger.info("create for path /acls result[{}]", result);
+
+        Stat rootStat = new Stat();
+        result = new String(client.getData().storingStatIn(rootStat).forPath("/"));
+        logger.info("getData for path / result[{}]", result);
+        logger.info("root path stat: {}", rootStat);
+
+        Stat stat= client.setData().withVersion(rootStat.getVersion()).forPath("/", "".getBytes());
+        logger.info("setData for path / result[{}]", stat);
+
+        stat = client.checkExists().inBackground(
+                new BackgroundCallback() {
+                    public void processResult(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
+
+                        logger.info("event=[{}], result code=[{}]", curatorEvent.getType(), curatorEvent.getResultCode());
+
+                        switch (curatorEvent.getType()) {
+                            case CREATE:
+                                logger.info("prdocess CREATE event");
+                                break;
+                            case DELETE:
+                                logger.info("process DELETE event");
+                                break;
+                            case EXISTS:
+                                logger.info("process EXISTS event");
+                                break;
+                            case GET_DATA:
+                                logger.info("process GET_DATA event");
+                                break;
+                            case SET_DATA:
+                                logger.info("process SET_DATA event");
+                                break;
+                            case CHILDREN:
+                                logger.info("process CHILDREN event");
+                                break;
+                            case SYNC:
+                                logger.info("process SYNC event");
+                                break;
+                            case GET_ACL:
+                                break;
+                            case SET_ACL:
+                                logger.info("process SET_ACL event");
+                                break;
+                            case WATCHED:
+                                logger.info("process WATCHED event");
+                                break;
+                            case CLOSING:
+                                logger.info("process CLOSING event");
+                                break;
+                        }
+                    }
+                }
+        ).forPath("/");
+        logger.info("checkExists for path / result[{}]", stat);
+
+
+        List<String> paths = client.getChildren().forPath("/");
+        logger.info("paths:{}", paths);
+
+
+        client.getData().usingWatcher(new Watcher() {
+            public void process(WatchedEvent event) {
+                logger.info("ZK Native Watcher");
+            }
+        }).forPath("/master");
+        client.getData().usingWatcher(new CuratorWatcher() {
+            public void process(WatchedEvent event) throws Exception {
+                logger.info("Curator Watcher");
+            }
+        }).forPath("/master");
+
+
+
+        /**
+         * NodeCache: 节点监听
+         *
+         * NodeCache不仅可以用于监听数据节点内容变更，也能监听指定节点是否存在。
+         * 如果原本节点不存在，那么cache就会在节点创建后触发NodeCacheListener,
+         * 但是如果该节点被删除，那么Curator就无法触发NodeCacheListener了。
+         */
+        final NodeCache nodeCache = new NodeCache(client, "/acl", false);
+        // 立即从ZK中获取数据并缓存
+        nodeCache.start(true);
+
+        nodeCache.getListenable().addListener(
+                new NodeCacheListener() {
+                    public void nodeChanged() throws Exception {
+                        logger.info("cache node data changed to [{}] ", new String(nodeCache.getCurrentData().getData()));
+                    }
+                }
+        );
+        client.setData().forPath("/acl", "test cache node listener".getBytes());
+        // 使用原生监听器
+//        client.getData().usingWatcher(new Watcher() {
+//            public void process(WatchedEvent watchedEvent) {
+//
+//            }
+//        });
+
+        /**
+         * PathChildrenCache：子节点监听
+         *
+         */
+        try {
+            result = client.create().withMode(CreateMode.PERSISTENT).forPath("/nodes", "nodes".getBytes());
+            logger.info("create for path / result[{}]", result);
+        } catch (KeeperException.NodeExistsException e) {
+            logger.info("znode /nodes exists");
+        }
+
+        result = client.create().withMode(CreateMode.EPHEMERAL).forPath("/nodes/node1", "node1".getBytes());
+        logger.info("create for path / result[{}]", result);
+
+        final PathChildrenCache pathChildrenCache = new PathChildrenCache(client, "/nodes/node1", true);
+        /**
+         * 1. POST_INITIALIZED_EVENT：异步初始化cache，初始化完成后会出发事件INITIALIZED；
+         * 2. NORMAL：异步初始化cache，在监听器启动的时候会枚举当前路径所有子节点，触发CHILD_ADDED类型的事件；
+         * 3. BUILD_INITIAL_CACHE：同步初始化客户端的cache，即创建cache后，就从服务器端拉入对应的数据；
+         */
+        pathChildrenCache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
+
+        pathChildrenCache.getListenable().addListener(
+                new PathChildrenCacheListener() {
+
+                    /**
+                     * 1. 注册子节点触发 CHILD_ADDED
+                     * 2. 更新子节点值触发 CHILD_UPDATED
+                     * 3. 删除子节点触发 CHILD_REMOVED
+                     * 4. ZK挂掉触发CONNECTION_SUSPENDED，一段时间后触发CONNECTION_LOST
+                     * 5. ZK重启触发CONNECTION_RECONNECTED
+                     */
+                    public void childEvent(CuratorFramework client, PathChildrenCacheEvent event) throws Exception {
+                        switch (event.getType()) {
+                            case CHILD_ADDED:
+                                logger.info("process CHILD_ADDED event");
+                                break;
+                            case CHILD_UPDATED:
+                                logger.info("process CHILD_UPDATED event");
+                                break;
+                            case CHILD_REMOVED:
+                                logger.info("process CHILD_REMOVED event");
+                                break;
+                            case CONNECTION_SUSPENDED:
+                                logger.info("process CONNECTION_SUSPENDED event");
+                                break;
+                            case CONNECTION_RECONNECTED:
+                                logger.info("process CONNECTION_RECONNECTED event");
+                                break;
+                            case CONNECTION_LOST:
+                                logger.info("process CONNECTION_LOST event");
+                                break;
+                            case INITIALIZED:
+                                logger.info("process INITIALIZED event");
+                                break;
+                        }
+                    }
+                }
+        );
+
+        /**
+         *
+         * maxDepth值设置说明，比如当前监听节点/t1，目录最深为/t1/t2/t3/t4,则maxDepth=3,说明下面3级子目录全
+         * 监听，即监听到t4，如果为2，则监听到t3,对t3的子节点操作不再触发
+         */
+        final TreeCache  treeCache =  TreeCache.newBuilder(client, "/tasks")
+                                               .setCacheData(true)
+                                               .setMaxDepth(2)
+                                               .build();
+        treeCache.start();
+        treeCache.getListenable().addListener(
+                new TreeCacheListener() {
+                    public void childEvent(CuratorFramework client, TreeCacheEvent event) throws Exception {
+                        switch (event.getType())  {
+                            case NODE_ADDED:
+                                logger.info("process NODE_ADDED event[{}]", event.getData());
+                                break;
+                            case NODE_UPDATED:
+                                logger.info("process NODE_UPDATED event");
+                                break;
+                            case NODE_REMOVED:
+                                logger.info("process NODE_REMOVED event");
+                                break;
+                            case CONNECTION_SUSPENDED:
+                                logger.info("process CONNECTION_SUSPENDED event");
+                                break;
+                            case CONNECTION_RECONNECTED:
+                                logger.info("process CONNECTION_RECONNECTED event");
+                                break;
+                            case CONNECTION_LOST:
+                                logger.info("process CONNECTION_LOST event");
+                                break;
+                            case INITIALIZED:
+                                logger.info("process INITIALIZED event");
+                                break;
+                        }
+                    }
+                }
+        );
+
+        client.create().withMode(CreateMode.EPHEMERAL).forPath("/tasks/task1", "task1".getBytes());
+
+
+    }
+
+}
---
+++ zk-in-action/src/main/java/govind/zkinaction/curator/UnifyConfig.java
@@ -0,0 +1,113 @@
+package govind.zkinaction.curator;
+
+import com.google.gson.Gson;
+import govind.zkinaction.curator.model.RedisConfig;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+public class UnifyConfig {
+    private static final Logger logger = LoggerFactory.getLogger(UnifyConfig.class);
+    final static String CONFIG_PATH = "/redis";
+    final static String SUB_CONFIG_PATH = "/config";
+    final static String CONN_STR="10.233.87.241:9080,10.233.87.54:9080";
+    final static int SESSION_TIMEOUT = 2000;
+    final static int CONN_TIMEOUT = 5000;
+
+    //用于挂起主进程
+    public static CountDownLatch latch =  new CountDownLatch(1);
+
+    private static CuratorFramework client = null;
+
+    public static void main(String[] args) {
+        try {
+
+            RetryPolicy retryPolicy = new ExponentialBackoffRetry(0, 10);
+
+             //启动Client
+             client = CuratorFrameworkFactory.builder()
+                    .retryPolicy(retryPolicy)
+                    .connectString(CONN_STR)
+                    .sessionTimeoutMs(SESSION_TIMEOUT)
+                    .connectionTimeoutMs(CONN_TIMEOUT)
+                    .build();
+             client.start();
+             logger.info("客户端连接成功，[{}]", client);
+
+            /**
+             * 采用PathChildrenCache对配置进行监控
+             */
+
+            final PathChildrenCache configNode = new PathChildrenCache(client, CONFIG_PATH,true);
+            configNode.start();
+
+            configNode.getListenable().addListener(
+              new PathChildrenCacheListener() {
+                  public void childEvent(CuratorFramework client, PathChildrenCacheEvent event) throws Exception {
+                      if (event.getType()== PathChildrenCacheEvent.Type.CHILD_UPDATED)  {
+                          // 确保发生数据更新的路径为/redis/config
+                          String pathChanged  = event.getData().getPath();
+                          if (pathChanged.equals(CONFIG_PATH+SUB_CONFIG_PATH)) {
+                              logger.info("[{}]配置发生改变，需要同步更新", pathChanged);
+
+                              //读取节点数据
+                              String jsonConfig = new String(event.getData().getData());
+                              logger.info("节点[{}]的配置为：{}", pathChanged, jsonConfig);
+
+                              //json配置转换为POJO
+                              RedisConfig redisConfig =  new Gson().fromJson(jsonConfig, RedisConfig.class);
+
+                              if (redisConfig !=  null) {
+                                  String type =  redisConfig.getType();
+                                  String url = redisConfig.getUrl();
+                                  String remark = redisConfig.getRemark();
+
+                                  if (type.equals("add")) {
+                                      logger.info("监听到新增配置，准备下载...");
+                                      Thread.sleep(1000);
+                                      logger.info("开始下载配置，下载路径为:[{}]", url);
+
+                                      Thread.sleep(2000);
+                                      logger.info("下载成功，已将其添加到项目中！");
+                                  } else if (type.equals("update")) {
+                                      logger.info("监听到更新配置，准备下载...");
+                                      Thread.sleep(1000);
+                                      logger.info("开始下载配置，下载路径为:[{}]", url);
+
+                                      Thread.sleep(2000);
+                                      logger.info("下载成功，准备替换配置...");
+                                      logger.info("备份项目已有配置");
+                                      logger.info("替换项目配置");
+                                      Thread.sleep(2000);
+                                      logger.info("配置成功！");
+                                  } else if (type.equals("delete")) {
+                                      logger.info("监听到需要删除配置");
+                                      Thread.sleep(1000);
+                                      logger.info("配置 删除成功");
+                                  }
+                              }
+                          }
+                      }
+                  }
+              }
+            );
+
+            latch.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (client != null) {
+                client.close();
+                logger.info("关闭会话");
+            }
+        }
+    }
+}
